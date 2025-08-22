4969dd122929852912ed9e4d21b7dd28443034de
Rohit Potdukhe <89935999+rohitpotdukhe01@users.noreply.github.com>
2025-08-22 19:27:04 +0200

    added config and gitignore file

---
+++ app/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-5")
+TWELVEDATA_API_KEY = os.getenv("TWELVEDATA_API_KEY", "")
+FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY", "")
+DEFAULT_PROVIDER = os.getenv("DEFAULT_PROVIDER", "yahoo")
